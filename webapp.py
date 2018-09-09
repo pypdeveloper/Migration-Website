e6c06f4545893b3b68b86f298544753e1890c49c
Ayush Deolasee <ayush@deolasee.org>
2018-09-09 03:19:45 +0000

    This is my first commit

---
+++ webapp.py
@@ -0,0 +1,22 @@
+#/bin/python
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "hello world"
+
+@app.route('/html')
+def html_index():
+    return "<!doctype html><html><head><title>HTML Page</title></head><body>Hello World</body></html>"
+
+@app.route('/animal')
+def animal_migration():
+    return ""
+
+if __name__== '__main__':
+    app.run(debug=True, host='0.0.0.0')
+
+
