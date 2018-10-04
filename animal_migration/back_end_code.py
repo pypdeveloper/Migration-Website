30210dba99da4c30b618365d6718f8f127e0f96e
Ayush Deolasee <ayush@deolasee.org>
2018-10-04 13:19:34 +0000

    website

--- animal_migration/back_end_code.py
+++ animal_migration/back_end_code.py
@@ -1,20 +1,21 @@
-from flask import Flask, render_templates 
+from flask import Flask, render_template 
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_templates('home.html')
+    return render_template('home.html')
 
-@app.route('/human')
+@app.route('/human_migration')
 def html_index():
-    return render_templates('human_migration')
+    return render_template('human_migration.html')
 
-@app.route('/animal')
+@app.route('/animal_migration')
 def animal_migration():
-    return render_templates('my_html_code.html')
+    return render_template('my_html_code.html')
 
 if __name__== '__main__':
     app.run(debug=True, host='0.0.0.0')
+
 
 
