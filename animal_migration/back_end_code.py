e3222d4fae74d329b72461e384abb4e27d56a34e
pypdeveloper <ayush@deolasee.org>
2018-11-02 10:41:52 +0530

    removing the port attrabute

--- animal_migration/back_end_code.py
+++ animal_migration/back_end_code.py
@@ -15,7 +15,7 @@ def animal_migration():
     return render_template('my_html_code.html')
 
 if __name__== '__main__':
-    app.run(debug=True, host='0.0.0.0',  port=80)
+    app.run(debug=True, host='0.0.0.0',)
 
 
 
